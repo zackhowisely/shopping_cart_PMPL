b13ee56877dd053048e9f1f9146f2dfe87957898
zackhowisely <55075699+zackhowisely@users.noreply.github.com>
2025-01-13 11:24:07 +0700

    Replace the unused loop index count with _ (#9)

--- shopping_cart/extras.py
+++ shopping_cart/extras.py
@@ -7,5 +7,5 @@ from shopping_cart.models import OrderItem
 
 def generate_order_id():
     date_str = date.today().strftime('%Y%m%d')[2:] + str(datetime.datetime.now().second)
-    rand_str = "".join([random.choice(string.digits) for count in range(3)])
+    rand_str = "".join([random.choice(string.digits) for _ in range(3)])
     return date_str + rand_str
